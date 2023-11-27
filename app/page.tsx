ec650391235ec511385d3b66f20f78cca18c0a16
zak1999 <zakariyya_ahmed@outlook.com>
2023-11-27 19:43:12 +0000

    feat: successfully fetch all station

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-export default function Home() {
+const collectAllStationData = async () => {
+  const res = await fetch ('https://75750e17-4c6f-43f1-9a65-e4290c99700a.mock.pstmn.io/stations')
+  if (!res.ok){
+    throw new Error('There was an issue collecting the stations')
+  }
+  return res.json();
+};
 
+type Station = {
+  id: string;
+  logo: string;
+  name: string;
+  slug: string;
+};
+
+export default async function Home() {
+
+  // This runs server-side (https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating) 
+  const allStations = (await collectAllStationData()) as Station;
+  console.log(allStations);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="">
@@ -8,3 +27,4 @@ export default function Home() {
     </main>
   );
 }
+
