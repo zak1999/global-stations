ded8fc502dff7637e0df42dd8b7aa2aed98fef8a
zak1999 <zakariyya_ahmed@outlook.com>
2023-11-27 21:07:10 +0000

    feat: absctract StationCard to seperate file - client comp

---
+++ app/components/StationCard.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { FC, useState } from "react";
+import { Station } from "../page";
+import { getStationData } from "../api";
+
+const StationCard: FC<{ station: Station }> = ({ station }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [extraData, setExtraData] = useState<Station | null>(null);
+
+  const collectMoreInfo = async () => {
+    // if the data has beeen collected for this specific station, we have it in state and dont need to refetch
+    if (extraData !== null) return;
+
+    setLoading(true);
+    const xtraData = (await getStationData(station.slug)) as Station;
+    setExtraData(xtraData);
+    setLoading(false);
+  };
+
+  return (
+    <div className="rounded-xl bg-slate-50 border border-gray-200 shadow-md p-4 flex flex-col sm:flex-row items-center space-x-4 justify-between flex-nowrap">
+      {/* Logo */}
+      <div className="bg-white order-first flex flex-col items-center h-18 w-18 sm:w-24 sm:h-24 overflow-hidden rounded-full">
+        <img
+          src={station.logo}
+          alt={`${station.name}'s logo`}
+          className="sm:w-24 sm:h-24 mb-3 rounded-full shadow-lg"
+        />
+      </div>
+
+      {/* Station Info */}
+      <div className="flex flex-col w-full items-center sm:items-start">
+        <div className="flex justify-between bg-white w-full p-2 rounded-t-lg">
+          <h2 className="text-xl font-semibold ">{station.name}</h2>
+          {/* 'Extra' Btn */}
+          <button
+            onClick={() => {
+              collectMoreInfo();
+              setIsDropdownOpen(!isDropdownOpen);
+            }}
+            className=" hover:bg-slate-200 text-gray-600 px-5 text-lg font-bold"
+          >
+            {isDropdownOpen ? "-" : "+"}{" "}
+          </button>
+        </div>
+
+        {/* Extra Info Tab */}
+        {isDropdownOpen &&
+          (loading ? (
+            <div className="flex bg-red-50 w-full items-center justify-center">
+              <Loader />
+            </div>
+          ) : (
+            <div className=" bg-slate-100 w-full justify-self-end py-6 text-sm font-mono border-t-2">
+              <p className="capitalize">- "{extraData?.tagline}"</p>
+              <p className="">
+                - Listen{" "}
+                <a className="underline" href={`/${station.slug}`}>
+                  here
+                </a>
+              </p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+export default StationCard;
+
+
+// this is from here: https://reactsvgicons.com/
+// I added tailwind animate and changed the height/width
+const Loader = () => {
+  return (
+    <svg
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      height="2em"
+      width="2em"
+      className="animate-spin"
+    >
+      <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z" />
+    </svg>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
-import { FC } from "react";
+import StationCard from "./components/StationCard";
+import { collectAllStationData } from "./api";
 
-const collectAllStationData = async () => {
-  const res = await fetch(
-    "https://75750e17-4c6f-43f1-9a65-e4290c99700a.mock.pstmn.io/stations"
-  );
-  if (!res.ok) {
-    throw new Error("There was an issue collecting the stations");
-  }
-  return res.json();
-};
-
-type Station = {
+export type Station = {
   id: string;
   logo: string;
   name: string;
   slug: string;
+  tagline?: string;
+  streamUrl?: string;
 };
 
 export default async function Home() {
@@ -37,22 +30,3 @@ export default async function Home() {
     </main>
   );
 }
-
-const StationCard: FC<{ station: Station }> = ({ station }) => {
-  return (
-    <div className="rounded-xl bg-slate-50 border border-gray-200 shadow-md p-4 flex flex-col sm:flex-row items-center space-x-4 justify-between flex-nowrap">
-      <div className="bg-white h-18 w-18 sm:w-24 sm:h-24 overflow-hidden rounded-full">
-        <img
-          src={station.logo}
-          alt={`${station.name}'s logo`}
-          className="object-contain w-full h-full"
-        />
-      </div>
-      <div className="flex flex-col w-full">
-        <a href={`/${station.slug}`}>
-          <h2 className="text-xl font-semibold">{station.name}</h2>
-        </a>
-      </div>
-    </div>
-  );
-};
