a4424589a49817f6643396108f8ce6719eba829d
zak1999 <zakariyya_ahmed@outlook.com>
2023-11-27 19:49:09 +0000

    feat: new component -StationCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { FC } from "react";
+
 const collectAllStationData = async () => {
   const res = await fetch ('https://75750e17-4c6f-43f1-9a65-e4290c99700a.mock.pstmn.io/stations')
   if (!res.ok){
@@ -16,15 +18,23 @@ type Station = {
 export default async function Home() {
 
   // This runs server-side (https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating) 
-  const allStations = (await collectAllStationData()) as Station;
+  const allStations = (await collectAllStationData()) as Station[];
   console.log(allStations);
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="">
-        Global's Stations
-      </h1>
+      <h1 className="">Global's Stations</h1>
+      <ul>
+        {allStations.map((stat, index) => (
+          <li key={`${stat.id}_${index}`}>
+            <StationCard station={stat} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
 
+const StationCard: FC<{ station: Station }> = ({ station }) => {
+return <a>{station.name}</a>;
+}
